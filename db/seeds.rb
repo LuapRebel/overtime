ddc8f23e4c9dd1d40f4d47df1d0aa07ee6c9aa4a
howard <paulleber23@gmail.com>
2017-12-31 09:16:10 -0800

    Brought in live data for view page

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ puts "1 admin user created"
 
 100.times do |post|
   Post.create!(date: Date.today, 
-                rationale: "#{post} rationale content", 
+                rationale: "#{post} rationale content Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", 
                 user_id: @user.id, 
                 overtime_request: 2.5)
 end
