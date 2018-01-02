a9b3517d63f2d770551811b0f134ed21b496b223
howard <paulleber23@gmail.com>
2018-01-02 06:12:36 -0800

    Built out rake task for sending sms messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf", 
                     first_name: "Jon", 
                     last_name: "Snow",
                     phone: "5555555555")
-puts "1 user created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com", 
                   password: "asdfasdf", 
@@ -14,17 +14,17 @@ AdminUser.create(email: "admin@test.com",
                   phone: "5555555555")
 puts "1 admin user created"
 
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created."
+
 100.times do |post|
   Post.create!(date: Date.today, 
                 rationale: "#{post} rationale content Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", 
-                user_id: @user.id, 
+                user_id: @employee.id, 
                 overtime_request: 2.5)
 end
 
 puts "100 posts have been created."
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-puts "3 audit logs have been created."
