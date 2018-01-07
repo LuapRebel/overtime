f1537c5ca46f7c0a75d1526aa4d86431681ab0fa
howard <paulleber23@gmail.com>
2018-01-07 06:12:53 -0800

    Added update callback for audit logs to set confirm date

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,9 @@ puts "3 audit logs have been created."
 
 100.times do |post|
   Post.create!(date: Date.today, 
-                rationale: "#{post} rationale content Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", 
-                user_id: @employee.id, 
-                overtime_request: 2.5)
+              rationale: "#{post} rationale content Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", 
+              user_id: @employee.id, 
+              overtime_request: 2.5)
 end
 
 puts "100 posts have been created."
